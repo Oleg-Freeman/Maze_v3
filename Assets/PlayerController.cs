2ec3b4a4dba122d683e4468002dcb0306d3936d4
Oleg-Freeman <47694131+Oleg-Freeman@users.noreply.github.com>
2019-03-02 10:41:26 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0, ver) * speed * Time.deltaTime;
+
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
